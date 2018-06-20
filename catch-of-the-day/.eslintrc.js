270e3f473868d995d9d8179017d739e058e3c497
browningl <lyndsey.browning@vizolution.co.uk>
2018-06-20 09:33:02 +0100

    Set up AddFishForm

--- catch-of-the-day/.eslintrc.js
+++ catch-of-the-day/.eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: 'airbnb',
+  parser: 'babel-eslint',
   rules: {
     'react/prefer-stateless-function': ['off'],
     'react/jsx-filename-extension': ['off'],
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends Component {
+  myInput = React.createRef();
+
+  goToStore = (e) => {
+    e.preventDefault();
+
+    const storeName = this.myInput.value.value;
+
+    this.props.history.push(`/store/${storeName}`);
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please enter a store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={this.myInput} />
         <button type="submit">Visit Store</button>
       </form>
     );
