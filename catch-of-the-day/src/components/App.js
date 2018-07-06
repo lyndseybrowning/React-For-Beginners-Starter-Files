f46f96cc1b89a8dbfe2654c00d4860930a4ec470
browningl <lyndsey.browning@vizolution.co.uk>
2018-07-06 08:53:22 +0100

    Load sample fishes and display order

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish';
+import sampleFishes from '../sample-fishes';
 
 class App extends Component {
 
@@ -20,14 +22,35 @@ class App extends Component {
     });
   };
 
+  loadSampleFishes = () => {
+    this.setState({
+      fishes: sampleFishes,
+    });
+  };
+
+  addToOrder = (key) => {
+    const order = { ...this.state.order };
+
+    order[key] = order[key] + 1 || 1;
+
+    this.setState({ order });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {
+              Object.keys(this.state.fishes).map((key) => {
+                return <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />;
+              })
+            }
+          </ul>
         </div>
-        <Order />
-        <Inventory addFish={this.addFish} />
+        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
       </div>
     );
   }
--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,8 +1,49 @@
 import React, { Component } from 'react';
+import { formatPrice } from '../helpers';
 
 class Order extends Component {
+
+  renderOrder = (key) => {
+    const fish = this.props.fishes[key];
+    const count = this.props.order[key];
+    const isAvailable = fish.status === 'available';
+
+    if (!isAvailable) {
+      return <li key={key}>Sorry {fish ? fish.name : 'fish'} is no longer available</li>
+    }
+
+    return (
+      <li key={key}>
+        {count} lbs {fish.name}
+        {formatPrice(count * fish.price)}
+      </li>
+    );
+  };
+
   render() {
-    return <div className="order">Order</div>;
+    const { fishes, order } = this.props;
+    const orderIds = Object.keys(order);
+    const total = orderIds.reduce((prevTotal, key) => {
+      const fish = fishes[key];
+      const count = order[key];
+      const isAvailable = fish && fish.status === 'available';
+
+      if (isAvailable) {
+        return prevTotal + (fish.price * count);
+      }
+
+      return prevTotal;
+    }, 0);
+
+    return (
+      <div className="order-wrap">
+        <h2>Your Order</h2>
+        <ul className="order">{ orderIds.map(this.renderOrder)}</ul>
+        <div className="total">
+          Total: <strong>{formatPrice(total)}</strong>
+        </div>
+      </div>
+    );
   }
 }
 
